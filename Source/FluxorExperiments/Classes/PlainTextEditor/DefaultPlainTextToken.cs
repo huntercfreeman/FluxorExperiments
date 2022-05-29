d7976b0929aa42845ff1db190a37e97612f2344e
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2022-05-29 13:53:02 -0400

    ImmutableStringBuilderRecord

--- Source/FluxorExperiments/Classes/PlainTextEditor/DefaultPlainTextToken.cs
+++ Source/FluxorExperiments/Classes/PlainTextEditor/DefaultPlainTextToken.cs
@@ -1,4 +1,5 @@
-﻿using FluxorExperiments.Classes.KeyDownEvent;
+﻿using FluxorExperiments.Classes.ImmutableStringBuilder;
+using FluxorExperiments.Classes.KeyDownEvent;
 using FluxorExperiments.Classes.Sequence;
 using System.Text;
 
@@ -8,21 +9,29 @@ public record DefaultPlainTextToken(PlainTextTokenKey PlainTextTokenKey, Sequenc
 	: PlainTextTokenBase(PlainTextTokenKey, SequenceKey)
 {
 	// TODO: DefaultPlainTextToken needs to be immutable look into perhaps Span<T>?
-	private readonly StringBuilder _contentBuilder;
+	private readonly ImmutableStringBuilderRecord _immutableStringBuilderRecord;
+	private readonly ImmutableStringBuilderRecordKey _immutableStringBuilderRecordKey;
 
 	public DefaultPlainTextToken(KeyDownEventRecord keyDownEventRecord)
 		: this(PlainTextTokenKey.NewPlainTextTokenKey(), SequenceKey.NewSequenceKey())
 	{
-		_contentBuilder = new(keyDownEventRecord.Key);
+		_immutableStringBuilderRecord = new();
+
+		_immutableStringBuilderRecordKey = _immutableStringBuilderRecord
+			.Append(keyDownEventRecord.Key);
 	}	
 	
 	public DefaultPlainTextToken(KeyDownEventRecord keyDownEventRecord,
 		DefaultPlainTextToken otherDefaultPlainTextToken)
 		: this(otherDefaultPlainTextToken.PlainTextTokenKey, SequenceKey.NewSequenceKey())
 	{
-		_contentBuilder = new(otherDefaultPlainTextToken.ToPlainText + keyDownEventRecord.Key);
+		_immutableStringBuilderRecord = otherDefaultPlainTextToken._immutableStringBuilderRecord;
+		
+		_immutableStringBuilderRecordKey = _immutableStringBuilderRecord
+			.Append(keyDownEventRecord.Key);
 	}	
 	
 	public override PlainTextTokenKind PlainTextTokenKind => PlainTextTokenKind.Default;
-	public override string ToPlainText => _contentBuilder.ToString();
+	public override string ToPlainText => _immutableStringBuilderRecord
+		.GetString(_immutableStringBuilderRecordKey);
 }
